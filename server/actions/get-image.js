58c2139be3944dc8bbfe9c2ed0f9a32239dfaf17
Sam Goodger <sam@goodger.nz>
2021-05-31 03:13:39 +1000

    update endpoints

--- server/actions/get-image.js
+++ server/actions/get-image.js
@@ -2,11 +2,12 @@ const db = require('../../database');
 
 let imageIds = [];
 
+const imageColumns = ['id', 'source', 'taken_at'];
+
 async function fetchImageIds() {
   try {
     const images = await db('images').select('id');
     imageIds = images.map(({ id }) => id);
-    console.log(imageIds);
   } catch (error) {
     console.error(error);
   }
@@ -18,11 +19,11 @@ async function fetchImageIds() {
 })();
 
 async function getImage(id) {
-  return !isNaN(parseInt(id)) ? db('images').where({ id }).first() : null;
+  return !isNaN(parseInt(id)) ? db('images').select(imageColumns).where({ id }).first() : null;
 }
 
 async function getImages(currentPage, sort) {
-  return db('images').orderBy('created_at', sort).paginate({ currentPage });
+  return db('images').select(imageColumns).orderBy('created_at', sort).paginate({ currentPage, isLengthAware: true });
 }
 
 async function getRandomImage() {
@@ -31,11 +32,11 @@ async function getRandomImage() {
 }
 
 async function getLatestImage() {
-  return db('images').whereNotNull('taken_at').orderBy('taken_at', 'desc').first();
+  return db('images').select(imageColumns).whereNotNull('taken_at').orderBy('taken_at', 'desc').first();
 }
 
 async function getLatestImages(currentPage, sort) {
-  return db('images').whereNotNull('taken_at').orderBy('taken_at', sort).paginate({ currentPage });
+  return db('images').select(imageColumns).whereNotNull('taken_at').orderBy('taken_at', sort).paginate({ currentPage, isLengthAware: true });
 }
 
 module.exports = {
