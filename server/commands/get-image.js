7215754f0b203232afdfcab0d5aa08a096b149ed
Sam Goodger <sam@goodger.nz>
2021-10-30 12:10:33 +1100

    Fix timestamp

--- server/commands/get-image.js
+++ server/commands/get-image.js
@@ -8,14 +8,14 @@ const { getImage } = require('../actions');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName(DISCORD_COMMAND)
-    .setDescription('Replies with MEOW')
+    .setDescription(`Sends a picture of ${DISCORD_COMMAND}`)
     .addStringOption(option => option.setName('id').setDescription('The ID of the image you want, or "latest"').setRequired(false)),
   async execute(interaction) {
     const id = interaction.options.getString('id');
 
     const image = await getImage(id);
 
-    const embed = new MessageEmbed().setImage(image.source_medium).setTimestamp(new Date(image.taken_at)).setFooter(`ID: ${image.id}}`);
+    const embed = new MessageEmbed().setImage(image.source_medium).setTimestamp(image.taken_at).setFooter(`ID: ${image.id}`);
 
     await interaction.reply({ embeds: [ embed ]});
   }
